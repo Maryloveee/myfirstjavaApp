1cc9d94d477fcaecdcf08ae4be2faaa23023c640
CL2~PC19 <CL2~PC19@DESKTOP-IR5C68U>
2025-08-15 12:06:11 +0800

    activity111

--- src/main/java/com/mycompany/myfirstjavaapp/MyfirstjavaApp.java
+++ src/main/java/com/mycompany/myfirstjavaapp/MyfirstjavaApp.java
@@ -3,6 +3,7 @@
  */
 
 package com.mycompany.myfirstjavaapp;
+import java.util.Scanner;
 
 /**
  *
@@ -11,6 +12,45 @@ package com.mycompany.myfirstjavaapp;
 public class MyfirstjavaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner input = new Scanner (System.in);
+        System.out.println("Enter Number 1:");
+        int x = Integer.parseInt(input.nextLine());
+        System.out.println("Enter Number 2:");
+        int y = Integer.parseInt(input.nextLine());
+        
+        int sum = x + y;
+        System.out.println("The sum is: " + sum);
+        
+        int rock = 1;
+        int paper = 2;
+        int scissor = 3;
+        
+        System.out.println("1.ROCK");
+        System.out.println("2.PAPER");
+        System.out.println("3.SCISSOR");
+        
+        System.out.println("Player 1 select Option:");
+        int player1 = Integer.parseInt(input.nextLine());
+        
+        System.out.println("Player 2 select Option:");
+        int player2 = Integer.parseInt(input.nextLine());
+        
+        if (player1 == 1 && player2 ==1)
+        {
+           System.out.println("DRAW");
+        }
+        else if (player1 == 1 && player2 == 2 || player1 == 2 && player2 == 3 || player1 == 3 && player2 ==1)
+        {
+            System.out.println("WIN PLAYER 2");
+        }
+        
+        else if (player1 == 2 && player2 == 1 || player1 == 3 && player2 == 2 || player1 == 1 && player2 ==3)
+        {
+            System.out.println("WIN PLAYER 1");
+        }
+        else
+        {
+            System.out.println("INVALID CHOICES");
+        }
     }
 }
